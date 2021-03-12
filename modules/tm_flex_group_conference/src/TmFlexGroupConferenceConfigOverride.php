42e6a7713689843fd952ce5793cdf88a2b5edfc0
globenewmedialtd <globenewmedialtd@gmail.com>
2021-03-12 15:11:19 +0100

    New Group Conference added

---
+++ modules/tm_flex_group_conference/src/TmFlexGroupConferenceConfigOverride.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace Drupal\tm_flex_group_conference;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+
+/**
+ * Class SocialGroupSecretConfigOverride.
+ *
+ * @package Drupal\social_group_secret
+ */
+class TmFlexGroupConferenceConfigOverride implements ConfigFactoryOverrideInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs the configuration override.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Load overrides.
+   */
+  public function loadOverrides($names) {
+    $overrides = [];    
+
+    $config_names = [
+      'search_api.index.social_all',
+      'search_api.index.social_groups',
+    ];
+
+    foreach ($config_names as $config_name) {
+      if (in_array($config_name, $names)) {
+        $overrides[$config_name] = [
+          'field_settings' => [
+            'rendered_item' => [
+              'configuration' => [
+                'view_mode' => [
+                  'entity:group' => [
+                    'tm_conference' => 'teaser',
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ];
+      }
+    }
+
+    $config_names = [
+      'views.view.search_all',
+      'views.view.search_groups',
+    ];
+
+    foreach ($config_names as $config_name) {
+      if (in_array($config_name, $names)) {
+        $overrides[$config_name] = [
+          'display' => [
+            'default' => [
+              'display_options' => [
+                'row' => [
+                  'options' => [
+                    'view_modes' => [
+                      'entity:group' => [
+                        'tm_conference' => 'teaser',
+                      ],
+                    ],
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ];
+      }
+    }
+
+    $config_names = [
+      'views.view.group_members',
+      'views.view.group_manage_members',
+    ];
+
+    foreach ($config_names as $config_name) {
+      if (in_array($config_name, $names)) {
+        $overrides[$config_name] = [
+          'dependencies' => [
+            'config' => [
+              'tm_conference-group_membership' => 'group.content_type.tm_conference-group_membership',
+            ],
+          ],
+          'display' => [
+            'default' => [
+              'display_options' => [
+                'filters' => [
+                  'type' => [
+                    'value' => [
+                      'tm_conference-group_membership' => 'tm_conference-group_membership',
+                    ],
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ];
+      }
+    }
+
+    $config_names = [
+      'views.view.group_events' => 'tm_conference-group_node-event',
+      'views.view.group_topics' => 'tm_conference-group_node-topic',
+    ];
+
+    foreach ($config_names as $config_name => $content_type) {
+      if (in_array($config_name, $names)) {
+        $overrides[$config_name] = [
+          'dependencies' => [
+            'config' => [
+              'group-content-type' => 'group.content_type.' . $content_type,
+              'group-type' => 'group.type',
+            ],
+          ],
+          'display' => [
+            'default' => [
+              'display_options' => [
+                'arguments' => [
+                  'gid' => [
+                    'validate_options' => [
+                      'bundles' => [
+                        'tm_conference' => 'tm_conference',
+                      ],
+                    ],
+                  ],
+                ],
+                'filters' => [
+                  'type' => [
+                    'value' => [
+                      $content_type => $content_type,
+                    ],
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ];
+      }
+    }
+
+    $config_name = 'block.block.views_block__group_managers_block_list_managers';
+
+    if (in_array($config_name, $names, FALSE)) {
+      $overrides[$config_name] = [
+        'visibility' => [
+          'group_type' => [
+            'group_types' => [
+              'tm_conference' => 'tm_conference',
+            ],
+          ],
+        ],
+      ];
+    }
+
+    $config_name = 'block.block.membershiprequestsnotification';
+
+    if (in_array($config_name, $names, FALSE)) {
+      $overrides[$config_name] = [
+        'visibility' => [
+          'group_type' => [
+            'group_types' => [
+              'tm_conference' => 'tm_conference',
+            ],
+          ],
+        ],
+      ];
+    }
+
+    $config_name = 'block.block.membershiprequestsnotification_2';
+
+    if (in_array($config_name, $names, FALSE)) {
+      $overrides[$config_name] = [
+        'visibility' => [
+          'group_type' => [
+            'group_types' => [
+              'tm_conference' => 'tm_conference',
+            ],
+          ],
+        ],
+      ];
+    }
+
+    $config_name = 'message.template.create_content_in_joined_group';
+    if (in_array($config_name, $names, FALSE)) {
+      $overrides[$config_name]['third_party_settings']['activity_logger']['activity_bundle_entities'] =
+        [
+          'group_content-tm_conference-group_node-event' => 'group_content-tm_conference-group_node-event',
+          'group_content-tm_conference-group_node-topic' => 'group_content-tm_conference-group_node-topic',
+        ];
+    }
+
+    $config_name = 'message.template.join_to_group';
+    if (in_array($config_name, $names, FALSE)) {
+      $overrides[$config_name]['third_party_settings']['activity_logger']['activity_bundle_entities'] =
+        [
+          'group_content-tm_conference-group_membership' => 'group_content-tm_conference-group_membership',
+        ];
+    }
+
+    $config_name = 'message.template.invited_to_join_group';
+    if (in_array($config_name, $names, FALSE)) {
+      $overrides[$config_name]['third_party_settings']['activity_logger']['activity_bundle_entities'] =
+        [
+          'group_content-tm_conference-group_invitation' => 'group_content-tm_conference-group_invitation',
+        ];
+    }
+
+    $config_name = 'message.template.approve_request_join_group';
+    if (in_array($config_name, $names, FALSE)) {
+      $overrides[$config_name]['third_party_settings']['activity_logger']['activity_bundle_entities'] =
+        [
+          'group_content-tm_conference-group_membership' => 'group_content-tm_conference-group_membership',
+        ];
+    }
+
+    $config_name = 'views.view.group_managers';    
+
+    return $overrides;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'TmFlexGroupConferenceConfigOverride';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+
+}
